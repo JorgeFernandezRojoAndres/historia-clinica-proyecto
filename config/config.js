205eead356ada3b69e7f924d09843514db16360e
JorgeFernandezRojoAndres <120122104+JorgeFernandezRojoAndres@users.noreply.github.com>
2024-10-07 00:20:35 -0300

    Configuración de base de datos con Clever Cloud

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   root: __dirname,
   db: {
-      database: 'medicappdb',
-      username: 'root',
-      password: '',
+      database: 'blqt94iqdvbk9eegbqps',
+      username: 'uvez2hmlbqpyconj',
+      password: 'GN0eLZBseDu8As97hLg9',
       options: {
-          host: 'localhost',
+          host: 'blqt94iqdvbk9eegbqps-mysql.services.clever-cloud.com',
       },
   },
 };
