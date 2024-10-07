cd5dc13c554cf3f5ba229b9d40326f07f60ebdf3
JorgeFernandezRojoAndres <120122104+JorgeFernandezRojoAndres@users.noreply.github.com>
2024-10-07 00:32:45 -0300

    Configuración de base de datos con Clever Cloud puerto 3006

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require('./config/config');
 const mysql = require('mysql2');
 // Inicializa la aplicación
 const app = express();
-const port = 3000;
+const port = 3306;
 
 // Configuración del motor de vistas
 app.use(express.static('public'));
